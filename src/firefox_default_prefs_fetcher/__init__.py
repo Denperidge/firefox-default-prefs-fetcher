f2ac022d4c62a9d4e64518f6b648e474fbc0699d
Denperidge <cat@denperidge.com>
2024-11-10 15:27:31 +0100

    First draft of CI, output filenames now include platform & Firefox version, disabled no_defaults output

--- src/firefox_default_prefs_fetcher/__init__.py
+++ src/firefox_default_prefs_fetcher/__init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from configparser import ConfigParser
 from os import name, makedirs
+from platform import system
 from shutil import copytree, ignore_patterns, rmtree
 from json import dumps, loads
 
@@ -8,6 +9,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
+EXPORT_NO_DEFAULTS = False
 OUT_DIR = "out/"
 FIREFOX_ROOT = Path.home().joinpath(".mozilla/firefox").absolute() if name != "nt" else Path(getenv("APPDATA") + "/Mozilla/Firefox/").resolve()
 PROFILE_NAME = "firefox-default-prefs-fetcher"
@@ -49,19 +51,28 @@ def create_new_profile():
     copytree(default_profile_folder, dest, ignore=ignore_patterns("*lock"))
 
 
+def create_prefix(platform, firefox_version):
+    return f"{platform}-{firefox_version}-"
+
 def main():
     makedirs(OUT_DIR, exist_ok=True)
     create_new_profile()
     options = webdriver.FirefoxOptions()
     options.profile = webdriver.FirefoxProfile(profile_directory=str(PROFILE_PATH))
     
-    options.add_argument("about:config")
-    print("Starting driver...")
+    options.add_argument("about:support")
     try:
+        print("Starting driver...")
         driver = webdriver.Firefox(options)
+        firefox_version = driver.find_element(By.ID, "version-box").text
+        platform = system().lower()
+        if platform == "":
+            print("Couldn't determined platform!")
+            exit(1)
+        prefix = create_prefix(platform=platform, firefox_version=firefox_version)
 
         print("Find")
-        #driver.get("about:config")
+        driver.get("about:config")
 
 
         # Show all preferences
@@ -74,21 +85,22 @@ def main():
         
         
         # Note custom/no-default/deprecated values
-        no_defaults = []
+        no_defaults_found = []
         delete_buttons = driver.find_elements(By.CLASS_NAME, "button-delete")
         for button in delete_buttons:
             key_container = button.find_element(By.XPATH, "..").find_element(By.XPATH, "..").find_element(By.CSS_SELECTOR, "[scope='row']")
             key_element = key_container.find_element(By.TAG_NAME, "span")
-            no_defaults.append(key_element.text)
+            no_defaults_found.append(key_element.text)
         
-        write_file("no_defaults.json", dumps(no_defaults))
+        if EXPORT_NO_DEFAULTS:
+            write_file(prefix + "no_defaults_found.json", dumps(no_defaults_found))
 
         default_preferences = []
 
         pref_rows = driver.find_element(By.ID, "prefs").find_elements(By.TAG_NAME, "tr")
         for pref_row in pref_rows:
             key = pref_row.find_element(By.TAG_NAME, "th").text
-            if key in no_defaults:
+            if key in no_defaults_found:
                 continue
             try:
                 # If this element doesn't throw NoSuchElement, it's a custom value that slipped through
@@ -107,8 +119,8 @@ def main():
             pref["key"] = key
             default_preferences.append(pref)
         
-        write_file("defaults.min.json", dumps(default_preferences))
-        write_file("defaults.json", dumps(default_preferences, indent=2))
+        write_file(prefix + "defaults.min.json", dumps(default_preferences))
+        write_file(prefix + "defaults.json", dumps(default_preferences, indent=2))
     except Exception as e:
         print(e)
     finally:

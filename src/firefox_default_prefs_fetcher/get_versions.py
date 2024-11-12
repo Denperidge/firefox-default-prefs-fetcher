26007b5b1a04dab286424de9c92be6de12a3ecf6
Denperidge <cat@denperidge.com>
2024-11-12 22:37:39 +0100

    Implemented firefox_newest_geckodriver.json

--- src/firefox_default_prefs_fetcher/get_versions.py
+++ src/firefox_default_prefs_fetcher/get_versions.py
@@ -59,25 +59,71 @@ def get_geckodriver_versions():
         
     write_file("geckodriver_versions.json", dumps(geckodriver_versions, indent=2))
     write_file("geckodriver_versions.min.json", dumps(geckodriver_versions))
+    
+    return geckodriver_versions
 
 
 def get_firefox_versions():
     hrefs = pq(url="https://ftp.mozilla.org/pub/firefox/releases/").find("table a")
-    versions = set()
+    firefox_versions = set()
     for href in hrefs:
         version = pq(href).text()
         if REGEX_MATCH_SEMANTIC_VERSIONING.match(version) is not None:
-            versions.add(version.replace("/", ""))
+            firefox_versions.add(version.replace("/", ""))
     
-    versions = list(versions)
+    firefox_versions = list(firefox_versions)
     # from https://stackoverflow.com/a/2574090
-    versions.sort(reverse=True, key=lambda v: [int(n) for n in v.replace("esr", "").split(".")])
+    firefox_versions.sort(reverse=True, key=lambda v: [int(n) for n in v.replace("esr", "").split(".")])
 
-    print(versions)        
+    write_file("firefox_versions.json", dumps(firefox_versions, indent=2))
+    write_file("firefox_versions.min.json", dumps(firefox_versions))
     
+    return firefox_versions
 
-
+def get_major_version(version):
+    return int(version.split(".")[0])
 
 def get_versions_main():
-    get_geckodriver_versions()
-    get_firefox_versions()
+
+    # These are both sorted from newest to oldest
+    geckodriver_versions = get_geckodriver_versions()
+    firefox_versions = get_firefox_versions()
+    firefox_newest_geckodriver = {}
+
+    for firefox_version in firefox_versions:
+        print()
+        print(f"--- Searching highest compatible Geckodriver for {firefox_version}---")
+        for geckodriver_version_data in geckodriver_versions:
+            geckodriver_version = geckodriver_version_data["geckodriver_version"]
+            # These values are major versions (e.g. 132, 10.0.4esr)
+            # The only available versions of these should be the ESR releases, as such it isn't relevant here
+
+            major_firefox_version = get_major_version(firefox_version)
+            min_major_firefox_version = int(geckodriver_version_data["min_firefox_version"].replace("ESR", ""))
+            max_major_firefox_version = None
+            if "max_firefox_version" in geckodriver_version_data.keys():
+                max_major_firefox_version = int(geckodriver_version_data["max_firefox_version"].replace("ESR", ""))
+                
+            
+            if major_firefox_version >= min_major_firefox_version:
+                print(f"geckodriver {geckodriver_version}: {firefox_version} is above minimum ({min_major_firefox_version})...")
+                
+                if max_major_firefox_version is None:
+                    print("... and has no maximum version!")
+                    firefox_newest_geckodriver[firefox_version] = geckodriver_version
+                    break
+                else:
+                    if major_firefox_version <= max_major_firefox_version:
+                        print(f"... and is under or equal to the maximum version ({max_major_firefox_version})")
+                        firefox_newest_geckodriver[firefox_version] = geckodriver_version
+                        break
+                    else:
+                        print(f"... and is over the maximum version ({max_major_firefox_version})")
+            else:
+                print(f"geckodriver {geckodriver_version}: {firefox_version} is below minimum ({min_major_firefox_version}). Skipping.")
+                continue
+        
+    
+    write_file("firefox_newest_geckodriver.min.json", dumps(firefox_newest_geckodriver))
+    write_file("firefox_newest_geckodriver.json", dumps(firefox_newest_geckodriver, indent=2))
+                    

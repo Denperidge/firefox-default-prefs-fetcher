ba03c2652aad21bc7f10a73ea0e16b689cd5266a
Denperidge <cat@denperidge.com>
2024-11-12 14:17:36 +0100

    WIP cli

--- src/firefox_default_prefs_fetcher/__init__.py
+++ src/firefox_default_prefs_fetcher/__init__.py
@@ -5,28 +5,29 @@ from platform import system
 from sys import argv
 from shutil import copytree, ignore_patterns, rmtree
 from json import dumps, loads
+from argparse import ArgumentParser
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-RUNNING_IN_CI = "--ci" in argv
-OUT_DIR = "out/"
-PROFILE_NAME = "firefox-default-prefs-fetcher"
-FIREFOX_ROOT = "" if RUNNING_IN_CI else Path.home().joinpath(".mozilla/firefox").absolute() if name != "nt" else Path(getenv("APPDATA") + "/Mozilla/Firefox/").resolve()
-PROFILE_PATH = "" if RUNNING_IN_CI else FIREFOX_ROOT.joinpath(PROFILE_NAME)
+DEFAULT_OUT_DIR = "out/"
+DEFAULT_PROFILE_NAME = "firefox-default-prefs-fetcher"
+DEFAULT_FIREFOX_DATA_DIR = Path.home().joinpath(".mozilla/firefox").absolute() if name != "nt" else Path(getenv("APPDATA") + "/Mozilla/Firefox/").resolve()
+DEFAULT_FIREFOX_PROFILE_PATH = DEFAULT_FIREFOX_DATA_DIR.joinpath(DEFAULT_PROFILE_NAME)
+
 
 
 
 def write_file(filename, data, out_dir=True):
     if out_dir:
-        filename = OUT_DIR + filename
+        filename = DEFAULT_OUT_DIR + filename
     with open(filename, "w", encoding="utf-8") as file:
         file.write(data)
 
 # Copied from yokoffing/Betterfox/install.py. But also I wrote that and I'm writing this for that
 def get_default_profile_folder():
-    config_path = FIREFOX_ROOT.joinpath("profiles.ini")
+    config_path = DEFAULT_FIREFOX_DATA_DIR.joinpath("profiles.ini")
     
     print(f"Reading {config_path}...")
 
@@ -37,18 +38,18 @@ def get_default_profile_folder():
         if "Default" in config_parser[section]:
             if config_parser[section]["Default"] == "1":
                 print("Default detected: " + section)
-                return FIREFOX_ROOT.joinpath(config_parser[section]["Path"])
+                return DEFAULT_FIREFOX_DATA_DIR.joinpath(config_parser[section]["Path"])
 
 
 def create_new_profile():
     default_profile_folder = get_default_profile_folder()
-    if PROFILE_PATH.exists():
-        print(f"{PROFILE_PATH} already exists.")
+    if DEFAULT_FIREFOX_PROFILE_PATH.exists():
+        print(f"{DEFAULT_FIREFOX_PROFILE_PATH} already exists.")
         print(f"You can optionally overwrite it with the contents of {default_profile_folder}")
-        if not input(f"Overwrite profile '{PROFILE_NAME}' [N/y]?: ").lower().strip().startswith("y"):
+        if not input(f"Overwrite profile '{DEFAULT_PROFILE_NAME}' [N/y]?: ").lower().strip().startswith("y"):
             return    
 
-    dest = str(Path(default_profile_folder).with_name(PROFILE_NAME))
+    dest = str(Path(default_profile_folder).with_name(DEFAULT_PROFILE_NAME))
     rmtree(dest)
     copytree(default_profile_folder, dest, ignore=ignore_patterns("*lock"))
 
@@ -57,14 +58,19 @@ def create_prefix(platform, firefox_version):
     return f"{platform}-{firefox_version}-"
 
 def main():
+    argparser = ArgumentParser()
+    argparser.add_argument("--ci", action="store_true", default=False, help="")
+    argparser.add_argument("--browser-version", "-bv", default=None, help="")
+    argparser.add_argument("-")
+
     driver = None
-    makedirs(OUT_DIR, exist_ok=True)
+    makedirs(DEFAULT_OUT_DIR, exist_ok=True)
     options = webdriver.FirefoxOptions()
 
     options.add_argument("about:config")
     if not RUNNING_IN_CI:
         create_new_profile()
-        options.profile = webdriver.FirefoxProfile(profile_directory=str(PROFILE_PATH))
+        options.profile = webdriver.FirefoxProfile(profile_directory=str(DEFAULT_FIREFOX_PROFILE_PATH))
     else:
         options.add_argument("--headless")
 
@@ -73,6 +79,7 @@ def main():
     try:
         print("Starting driver...")
         driver = webdriver.Firefox(options)
+        options.browser_version = 
         firefox_version = driver.capabilities["browserVersion"]  # Thanks to https://stackoverflow.com/a/58989044
         platform = system().lower()
         if platform == "":
